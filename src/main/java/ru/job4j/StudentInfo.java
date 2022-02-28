d66f4b4c08ad75fe884f2ccfa3e0eef45fdedb94
denis4ik23 <denis4ik2387@gmail.com>
2022-03-01 01:58:33 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Denis Cheklov");
+        System.out.println("23.08.1987");
+    }
+}
