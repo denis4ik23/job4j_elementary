8ff3a033de219d80a64b437c8f5f7b29954366fe
denis4ik23 <denis4ik2387@gmail.com>
2022-07-05 00:14:37 +0300

    6.4.0. Слово начинается с ... [#41585 #318290]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,18 +3,11 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int indexWord = 0; indexWord < word.length; indexWord++) {
-            if (indexWord == pref.length) {
-                break;
-            }
-            for (int indexPref = indexWord; indexPref < pref.length; indexPref++) {
-                if (word[indexWord] != pref[indexPref]) {
+        for (int i = 0; i < pref.length; i++) {
+                if (word[i] != pref[i]) {
                     result = false;
                     break;
-                } else if (word[indexWord] == pref[indexPref]) {
-                    break;
                 }
-            }
         }
         return result;
     }
