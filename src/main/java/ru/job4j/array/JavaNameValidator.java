36829123963e8a008ef72f95df16277a4b2ee5c8
denis4ik23 <denis4ik23@gmail.com>
2023-12-10 18:17:47 +0300

    3. Перегрузить метод max для трех чисел [#122644 #344213]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        return false;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return false; //codePointAt(code) == 36 || codePointAt(code) == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return false;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return false;
+    }
+}
